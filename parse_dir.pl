24db341c17f9343c8d9fbe001385a31ddcc4052d
Bill Parker-Combes <parkercombes@gmail.com>
2017-09-07 22:59:39 -0400

    Add files via upload
    Use Tkx for file chooser dialog, generically open pptx (zip archive) to "pptx_temp" folder name and traverse, building XML code that can be opened in FreeMind (mind mapping software) as new.mm (same directory as pptx file).  Also have cleaned up documentation and development code fragments.

--- parse_dir.pl
+++ parse_dir.pl
@@ -1,45 +1,87 @@
-#!/usr/bin/perl
+#!/usr/local/ActivePerl-5.24/bin/perl
 
-use warnings;
-use strict; 
-use File::Find; #core function of Perl
-
-use XML::Writer; #need to add with CPAN
-
-#currently hard coded, can be passed in later on command line (see following comment)
-my $start_dir = "/Users/BPC/workspace/slidegen/temp_v2";
-#my $start_dir = "/Users/BPC/workspace";
-
-#OR die "Usage: $0 DIRs" if not @ARGV;
-
-our $writer = XML::Writer->new(OUTPUT => 'self', DATA_MODE => 1, DATA_INDENT => 2, UNSAFE => 1 );
-$writer->xmlDecl('UTF-8');
-$writer->startTag('node', TEXT => $start_dir, LOCALIZED_STYLE_REF=>"AutomaticLayout.level.root", FOLDED=>"false");
-
-#insert the styletags from FreePlane
-fpmapstyle();
+use warnings; #always
+use strict;   #always
+use File::Find; #core function of Perl to recursively walk through directory structures safely
+use XML::Writer; #or writing valid XML
+use Tkx; #Gui toolkit for choose file dialog (newer Tkx API is much better than crufty Tk)
+use Archive::Zip qw(:ERROR_CODES); #for zipping and unzipping the pptx file
 
 
-our $initdepth = -1;
-our $olddepth = -1;
-our $newdepth = -1;
-our $pathchar = "/"; 
-our @status = ();
+# 1. Get the pptx file
+	my $filename = Tkx::tk___getOpenFile();
+	#print "fname: $filename\n";
 
-#find function performs recursive call through all directories to wanted{} subroutine
-find(\&wanted, $start_dir);
+	# 1A. parse the directory where the file is
+	$filename =~ /^(.+)\/[^\/]+$/;
 
-print "stats: $status[0] $status[1] $status[2]\n";
+	my $selected_dir = $1; 
+	#print "sdir: $selected_dir\n";
+	#hard coded for dev purposes
+	#$selected_dir = "/Users/BPC/workspace/slidegen/temp_v2";
 
-foreach($status[0]..$status[2]) {$writer->endTag();}
+# 2. Open the pptx file and unzip it to a hardcoded directory name
+	openpptx($filename, $selected_dir);
+
+# 3. Traverse the pptx unzip directory and build the XML file for FreePlane consumption
 
 
-my $xml = $writer->end();
+	# 3A. Start building the XML file - a global variable so it can be appropriately built in sub wanted()
+	our $writer = XML::Writer->new(OUTPUT => 'self', DATA_MODE => 1, DATA_INDENT => 2); #, UNSAFE => 1 );
+	$writer->xmlDecl('UTF-8');
+	$writer->startTag('node', TEXT => $selected_dir, LOCALIZED_STYLE_REF=>"AutomaticLayout.level.root", FOLDED=>"false");
 
-print $xml;
+	# 3B. Insert the styletags from FreePlane
+	fpmapstyle();
 
+# 4. Use the Find function performs recursive call through all directories with the built-in wanted() subroutine
 
-#-----------start subroutine
+	#these global variables are needed to exist outside of wanted() 
+	our $initdepth = -1;
+	our $olddepth = -1;
+	our $newdepth = -1;
+	our $pathchar = "/"; 
+	our @status = ();  #this is needed to save the 3 depth variables at a point in time to determine how many closing tags are needed to complete the XML file and stay compliant
+
+	my $corrected_dir = $selected_dir . "/pptx_temp"; #only need to parse the new temp directory
+	find(\&wanted, $corrected_dir);
+
+	# 4B. The difference in depth values captured provides the ability to close any remaining open XML tags the correct number of times
+	#print "stats: $status[0] $status[1] $status[2]\n";
+	foreach($status[0]..$status[2]) {$writer->endTag();}
+
+	# 4C. Close out the XML file
+	my $xml = $writer->end();
+
+	# 4D. Write $xml to a file with a *.mm suffix so that it can be opened by FreeMind software
+	open(my $fhxml, ">", "$selected_dir/new.mm") || die "Cannot open file 'new.mm'.\n";
+	print $fhxml $xml;
+	close($fhxml);
+
+exit;
+###############               
+
+#-----------start subroutine openpptx
+sub openpptx {
+	my $zipName = shift;
+	my $dirName = shift;
+	print "zName: $zipName\n";
+
+	my $zip     = Archive::Zip->new();
+	
+	my $status  = $zip->read($zipName);
+	my @members = $zip->memberNames();
+	die "Read of $zipName failed\n" if $status != AZ_OK;
+	foreach (@members) {
+	    $zip->extractMember("$_", "$dirName/pptx_temp/$_");
+	}
+
+	print "unzip done\n";
+	return;
+}
+#-----------end subroutine openpptx
+
+#-----------start subroutine wanted
 sub wanted {
 #	print "start\n";
 my $level=1;
@@ -88,13 +130,36 @@ my $level=1;
 #filenames are processed here		
 		
 	$writer->startTag('node', TEXT=> $_, LOCALIZED_STYLE_REF=>"styles.subsubtopic", POSITION=>"right" );
-	$writer->endTag();
 	#print "file\t$_\n";
+	my $fname = $_;
 	
+	open(my $fh, "<", "$fname") || die "Cannot open file '$fname'.\n";
+	my @lines = <$fh>;
+	close($fh);
+
+	#process @lines looking for references to other files using regex to match Target="(.+)"
+	
+	foreach my $line (@lines) {
+		if ($line =~ /Target\=\"([^\"]+)/) {
+			print "Target:$fname:$1\n";	
+			$writer->startTag('node', TEXT=> $1, LOCALIZED_STYLE_REF=>"AutomaticLayout.level,5", POSITION=>"right" );
+			$writer->startTag('font', ITALIC=>"true");
+			$writer->endTag();
+			$writer->endTag();
+			
+			#need code to link this current file to the targeted file...attribute DESTINATION="<some node id value>"
+#			<arrowlink SHAPE="CUBIC_CURVE" COLOR="#000000" WIDTH="2" TRANSPARENCY="200" FONT_SIZE="9" FONT_FAMILY="SansSerif" DESTINATION="ID_480159512" STARTINCLINATION="54;0;" ENDINCLINATION="54;0;" STARTARROW="NONE" ENDARROW="DEFAULT"/>
+						
+		} #end if
+	} #end foreach
+	
+	$writer->endTag();
+
 	return;
 }
-#-----------end subroutine wanted{}
+#-----------end subroutine wanted
 	
+#-----------start subroutine fpmapstyle
 sub fpmapstyle {
 	$writer->startTag('hook', NAME=>"MapStyle");
 		$writer->startTag('properties', fit_to_viewport=>"false;"); $writer->endTag();
@@ -149,7 +214,7 @@ sub fpmapstyle {
 						$writer->startTag('font', SIZE=>"10"); $writer->endTag();
 						$writer->startTag('edge', COLOR=>"#ff00ff"); $writer->endTag();
 					$writer->endTag();
-					$writer->startTag('stylenode', LOCALIZED_TEXT=>"AutomaticLayout.level,5");
+					$writer->startTag('stylenode', LOCALIZED_TEXT=>"AutomaticLayout.level,5", COLOR=>"#9228a8");
 						$writer->startTag('edge', COLOR=>"#00ffff"); $writer->endTag();
 					$writer->endTag();
 					$writer->startTag('stylenode', LOCALIZED_TEXT=>"AutomaticLayout.level,6");
@@ -177,4 +242,4 @@ sub fpmapstyle {
 
 	return; 
 } 
-#-----------end subroutine fpmapstyle{}
+#-----------end subroutine fpmapstyle
